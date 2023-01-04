ae37357da5978ee6d51602d6002a6f3d8ba0af0b
AndreVeiga <soutomachado1@gmail.com>
2023-01-05 00:33:18 +0044

    add arquives from site

---
+++ cursos.js
@@ -0,0 +1,20 @@
+var cursos = [
+  {
+    nome: "Bacharel em Sistemas da Informação",
+    lugar: "UCS - Universidade de Caxias do Sul",
+    data: "agosto/2016",
+    certificado: "https://drive.google.com/file/d/1_uyR5Nh40MeiecZtZlpGvxh0v5ZQdgJb/view?usp=share_link"
+  },
+  {
+    nome: "Engenharia de software",
+    lugar: "Alura",
+    data: "agosto/2022",
+    certificado: "https://cursos.alura.com.br/user/elton-veiga/degree-engenharia-software-287214/certificate"
+  },
+  {
+    nome: "Design Patterns em Java I: boas práticas de programação",
+    lugar: "Alura",
+    data: "dezembro/2022",
+    certificado: "https://cursos.alura.com.br/user/elton-veiga/course/introducao-design-patterns-java/certificate"
+  },
+]
